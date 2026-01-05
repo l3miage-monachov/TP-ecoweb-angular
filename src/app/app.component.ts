b0bf92b7502f053cb06d8e305cdb4a0f6c3dd3e6
Vivian Monachon <vivian.monachon@atral.tech>
2026-01-05 10:41:10 +0100

    BP RWEB 0047 - Limiter le nombre de requêtes HTTP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -9,4 +9,14 @@ import { HeaderComponent } from './layout/header/header.component';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    // RWEB 0047 & RWEB 0053 : Polling inutile et blocage JS
+    setInterval(() => {
+      console.log('Polling inutile pour consommer de la batterie...');
+      const heavyCalculation = new Array(10000).fill(0).map(() => Math.random());
+    }, 500); // Toutes les 500ms
+  }
+}
